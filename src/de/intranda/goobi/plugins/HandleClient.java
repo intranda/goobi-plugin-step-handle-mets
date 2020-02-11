7bdca2d582684f0dc36cc9c75399a7a6596f43c9
Joel Segal <joel.segal@intranda.com>
2020-02-11 17:52:58 +0100

    With functioning calling, creating and altering handles.

---
+++ src/de/intranda/goobi/plugins/HandleClient.java
@@ -0,0 +1,309 @@
+package de.intranda.goobi.plugins;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+import org.apache.commons.lang.StringUtils;
+
+import net.handle.hdllib.AbstractMessage;
+import net.handle.hdllib.AbstractResponse;
+import net.handle.hdllib.AdminRecord;
+import net.handle.hdllib.AuthenticationInfo;
+import net.handle.hdllib.CreateHandleRequest;
+import net.handle.hdllib.CreateHandleResponse;
+import net.handle.hdllib.Encoder;
+import net.handle.hdllib.HandleException;
+import net.handle.hdllib.HandleResolver;
+import net.handle.hdllib.HandleValue;
+import net.handle.hdllib.ModifyValueRequest;
+import net.handle.hdllib.PublicKeyAuthenticationInfo;
+import net.handle.hdllib.ResolutionRequest;
+import net.handle.hdllib.ResolutionResponse;
+import net.handle.hdllib.SecretKeyAuthenticationInfo;
+import net.handle.hdllib.Util;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class HandleClient {
+
+    //----------------Static fields------------------
+
+    private String strPathPrivatePEM; // "/home/joel/Handles/zertifikate/21.T11998_USER28-priv.pem";
+    //    public static String strPathCertPEM = "/home/joel/Handles/zertifikate/21.T11998_USER28-cert.pem";
+
+    private static String strUserHandle = "21.T11998/USER28";
+    private static String strHandleBase = "21.T11998";
+    private static String strURLPrefix = "https://viewer.goobi.io/idresolver?handle=";
+    private static int ADMIN_INDEX = 300;   //NOT 28!
+    private static int ADMIN_RECORD_INDEX = 100;
+    private static int URL_RECORD_INDEX = 1;
+
+    //----------------Non-Static fields--------------
+
+    private PrivateKey privKey;
+    private PublicKeyAuthenticationInfo authInfo;
+
+    //-----------------Testing:---------------------
+    public static void main(String[] args) throws HandleException, IOException {
+
+        HandleClient test = new HandleClient(args[0]);
+
+        //        test.resolveRequest("300:200/23");
+        //        test.resolveRequest("45678/1");
+        //        test.resolveRequest("200/23");
+        //      test.resolveRequest(21.T11998/TEST015c2702c8-d2ca-40b7-9220-400225b19cc3);
+        //        test.resolveRequest("10.1594/GFZ.ISDC.CHAMP/CH-ME-2-PLP");
+
+        test.resolveRequest("20.1000/100");
+        test.resolveRequest("21.T11998/TEMPLATEHANDLE");
+        test.resolveRequest(strUserHandle);
+
+        String strUniqueHandle = test.newURLHandle("21.T11998/TEST02", "https://stackoverflow.com/", false);
+//        test.changleHandleURL(strUniqueHandle, "https://www.theguardian.com/international");
+        test.resolveRequest(strUniqueHandle);
+        
+        test.resolveRequest("21.T11998/TEST02");
+        test.changleHandleURL("21.T11998/TEST02", "https://www.theguardian.com/international");
+        test.resolveRequest("21.T11998/TEST02");
+
+
+        //        
+    }
+
+    //---------------Ctor: get private key------------------
+    public HandleClient(String strPathPrivatePEM) throws HandleException, IOException {
+
+        this.strPathPrivatePEM = strPathPrivatePEM;
+        this.privKey = getPemPrivateKey();
+        this.authInfo = new PublicKeyAuthenticationInfo(Util.encodeString(strUserHandle), 300, privKey);
+    }
+
+    //Given an object with specified ID, make a handle "id_xyz" with URL given in getURLForHandle.
+    //Returns the new Handle.
+    public String makeURLHandleForObject(String strObjectId) throws HandleException {
+
+        String strNewHandle = newURLHandle(strHandleBase + "/" + strObjectId, strURLPrefix, true);
+        String strNewURL = getURLForHandle(strNewHandle);
+
+        if (changleHandleURL(strNewHandle, strNewURL)) {
+            return strNewHandle;
+        } else {
+            throw new HandleException(HandleException.INTERNAL_ERROR, "Failed to create new Handle for " + strObjectId);
+        }
+
+    }
+
+    //Make a new handle with specified URL.
+    //If boMintNewSuffix, add a suffix guaranteeing uniquness.
+    //Retuns the new handle.
+    public String newURLHandle(String strNewHandle, String url, Boolean boMintNewSuffix) throws HandleException {
+
+        if (!boMintNewSuffix && isHandleRegistered(strNewHandle)) {
+            resolveRequest(strNewHandle);
+            return strNewHandle;
+        }
+
+        // Define the admin record for the handle we want to create
+        AdminRecord admin = createAdminRecord(strUserHandle, ADMIN_INDEX);
+
+        // Make a create-handle request.
+        HandleValue values[] = { new HandleValue(ADMIN_RECORD_INDEX, // unique index
+                Util.encodeString("HS_ADMIN"), // handle value type
+                Encoder.encodeAdminRecord(admin)), //data
+
+                new HandleValue(URL_RECORD_INDEX, // unique index
+                        "URL", // handle value type
+                        url) }; //data
+
+        // Create the request to send and the resolver to send it
+        CreateHandleRequest request = new CreateHandleRequest(Util.encodeString(strNewHandle), values, authInfo);
+
+        //create a unique suffix?
+        if (boMintNewSuffix) {
+            request.mintNewSuffix = true;
+        }
+
+        HandleResolver resolver = new HandleResolver();
+        AbstractResponse response;
+
+        // Let the resolver process the request
+        response = resolver.processRequest(request);
+
+        // Check the response to see if operation was successful
+        if (response.responseCode == AbstractMessage.RC_SUCCESS) {
+
+            log.info(response);
+
+            byte[] btHandle = ((CreateHandleResponse) response).handle;
+            String strFinalHandle = Util.decodeString(btHandle);
+            log.info("Handle created: " + Util.decodeString(btHandle));
+
+            return strFinalHandle;
+
+        } else {
+            throw new HandleException(HandleException.INTERNAL_ERROR, "Failed trying to create a new handle at the server, response was" + response);
+        }
+    }
+
+    private String getURLForHandle(String strHandle) {
+
+        return strURLPrefix + strHandle;
+    }
+
+    public void resolveRequest(String strHandle) throws HandleException {
+
+        // Get the UTF8 encoding of the desired handle.
+        byte bytesHandle[] = Util.encodeString(strHandle);
+
+        // Create a resolution request.
+        // (without specifying any types, indexes)
+        ResolutionRequest request = new ResolutionRequest(bytesHandle, null, null, authInfo);
+
+        HandleResolver resolver = new HandleResolver();
+
+        AbstractResponse response = resolver.processRequest(request);
+
+        // Check the response to see if the operation was successful.
+        if (response.responseCode == AbstractMessage.RC_SUCCESS) {
+
+            // The resolution was successful, so we'll cast the response
+            // and get the handle values.
+            HandleValue values[] = ((ResolutionResponse) response).getHandleValues();
+            for (int i = 0; i < values.length; i++) {
+                log.info(String.valueOf(values[i]));
+            }
+        } else {
+            log.error(AbstractResponse.getResponseCodeMessage(response.responseCode) + " : " + strHandle);
+        }
+
+    }
+
+    //Change the URL for the handle. Returns true if successful, falso otherwise
+    public Boolean changleHandleURL(String handle, String newUrl) throws HandleException {
+
+        if (StringUtils.isEmpty(handle) || StringUtils.isEmpty(newUrl))
+            throw new IllegalArgumentException("DOI and URL cannot be empty");
+
+        log.info("update Handle: DOI: " + handle + " URL: " + newUrl);
+
+
+        try {
+            int timestamp = (int) (System.currentTimeMillis() / 1000);
+            HandleValue handleNew = new HandleValue(URL_RECORD_INDEX, "URL", newUrl);
+            handleNew.setTimestamp(timestamp);
+
+            // Make a create-handle request.
+            HandleValue values[] = {   handleNew };
+
+            ModifyValueRequest req = new ModifyValueRequest(Util.encodeString(handle), values, authInfo);
+
+            HandleResolver resolver = new HandleResolver();
+            AbstractResponse response = resolver.processRequest(req);
+
+            String msg = AbstractMessage.getResponseCodeMessage(response.responseCode);
+
+            log.info("response code from Handle request: " + msg);
+
+            if (response.responseCode != AbstractMessage.RC_SUCCESS) {
+                return false;
+            }
+
+        } catch (HandleException e) {
+            String message = "tried to update handle " + handle + " but failed: [" + e.getCode() + "] " + e.getMessage();
+            log.error(message);
+            throw e;
+        }
+
+        return true;
+    }
+
+    /**
+     * Create the NA admin record for a new handle. The NA admin is provided all permissions bar ADD_NA and DELETE_NA
+     * 
+     * @return AdminRecord an AdminRecord object representing the NA handle admin
+     * @param handle the NA admin handle in byte form
+     * @param idx the handle index the of the NA handle's HS_VLIST entry
+     */
+    public AdminRecord createAdminRecord(String handle, int idx) {
+        return new AdminRecord(Util.encodeString(handle), idx, AdminRecord.PRM_ADD_HANDLE, AdminRecord.PRM_DELETE_HANDLE, AdminRecord.PRM_NO_ADD_NA,
+                AdminRecord.PRM_NO_DELETE_NA, AdminRecord.PRM_READ_VALUE, AdminRecord.PRM_MODIFY_VALUE, AdminRecord.PRM_REMOVE_VALUE,
+                AdminRecord.PRM_ADD_VALUE, AdminRecord.PRM_MODIFY_ADMIN, AdminRecord.PRM_REMOVE_ADMIN, AdminRecord.PRM_ADD_ADMIN,
+                AdminRecord.PRM_LIST_HANDLES);
+    }
+
+    public PrivateKey getPemPrivateKey() throws HandleException, IOException {
+
+        File f = new File(strPathPrivatePEM);
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] keyBytes = new byte[(int) f.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+
+        String temp = new String(keyBytes);
+        String privKeyPEM = temp.replace("-----BEGIN PRIVATE KEY-----", "");
+        privKeyPEM = privKeyPEM.replace("-----END PRIVATE KEY-----", "");
+        privKeyPEM = privKeyPEM.replaceAll("\r\n", "");
+
+        byte[] decoded = Base64.getDecoder().decode(privKeyPEM);
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+
+        try {
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePrivate(spec);
+            
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new IOException("Failed to generate private key", e);
+        }
+    }
+
+    //registered?
+
+    public boolean isHandleRegistered(String handle) throws HandleException {
+
+        boolean handleRegistered = false;
+        ResolutionRequest req = buildResolutionRequest(handle);
+        AbstractResponse response = null;
+        HandleResolver resolver = new HandleResolver();
+        try {
+            response = resolver.processRequest(req);
+        } catch (HandleException ex) {
+            log.error("Caught exception trying to process lookup request");
+            ex.printStackTrace();
+            throw ex;
+        }
+        if ((response != null && response.responseCode == AbstractMessage.RC_SUCCESS)) {
+            log.info("Handle " + handle + " registered.");
+            handleRegistered = true;
+        }
+        return handleRegistered;
+    }
+
+    private ResolutionRequest buildResolutionRequest(final String handle) throws HandleException {
+
+        //find auth info for the whole domain:
+        String handlePrefix = handle.substring(0, handle.indexOf("/"));
+        PublicKeyAuthenticationInfo auth = new PublicKeyAuthenticationInfo(Util.encodeString(handlePrefix), 300, privKey);
+
+        byte[][] types = null;
+        int[] indexes = null;
+        ResolutionRequest req = new ResolutionRequest(Util.encodeString(handle), types, indexes, auth);
+        req.certify = false;
+        req.cacheCertify = true;
+        req.authoritative = false;
+        req.ignoreRestrictedValues = true;
+        return req;
+    }
+
+}
--- src/de/intranda/goobi/plugins/METSHandlePlugin.java
+++ src/de/intranda/goobi/plugins/METSHandlePlugin.java
@@ -5,12 +5,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import net.handle.hdllib.HandleException;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 import org.goobi.beans.Step;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.goobi.beans.LogEntry;
 import org.goobi.beans.Process;
+import org.goobi.beans.Ruleset;
 import org.goobi.production.enums.LogType;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginType;
@@ -29,12 +31,16 @@ import ugh.exceptions.MetadataTypeNotAllowedException;
 import ugh.exceptions.PreferencesException;
 import ugh.exceptions.ReadException;
 import ugh.exceptions.WriteException;
+import ugh.fileformats.mets.MetsMods;
+import uk.gov.nationalarchives.droid.core.signature.FileFormat;
 import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.helper.enums.StepEditType;
 import de.sub.goobi.helper.enums.StepStatus;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.metadaten.MetadatenHelper;
 import de.sub.goobi.persistence.managers.ProcessManager;
+import de.sub.goobi.persistence.managers.RulesetManager;
 import de.sub.goobi.persistence.managers.StepManager;
 import lombok.extern.log4j.Log4j;
 
@@ -42,16 +48,21 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class METSHandlePlugin implements IStepPlugin, IPlugin {
 
+    private String title = "intranda_step_handle_mets";
     private static final String PLUGIN_NAME = "HandleMETS";
+    private static final String PEM_FILE = "home/joel/Handles/zertifikate/21.T11998_USER28-priv.pem";
 
-    private static final String TITLE_PAGE_NAME = "TitlePage";
-    private static final String PAGE_NO_NAME = "physPageNumber";
-    private static final String REPRESENTATIVE_NAME = "_representative";
+    //for testing:
+    private static String rulesetExample = "/opt/digiverso/goobi/test/klassik.xml";
+    private static String xmlExample = "/opt/digiverso/goobi/test/meta.xml";
+    private static String xmlOut = "/opt/digiverso/goobi/test/metadataTEST.xml";
 
     private Process process;
     private Step step;
     private String returnPath;
     private Prefs prefs;
+
+    private MetadataType urn;
 
     @Override
     public PluginType getType() {
@@ -67,144 +78,136 @@ public class METSHandlePlugin implements IStepPlugin, IPlugin {
         return PLUGIN_NAME;
     }
 
+    //Testing:
+    public static void main(String[] args) throws Exception {
+
+        METSHandlePlugin plug = new METSHandlePlugin();
+
+        try {
+            plug.prefs = new Prefs();
+            plug.prefs.loadPrefs(rulesetExample);
+
+            //read the metatdata
+            Fileformat fileformat = new MetsMods(plug.prefs);
+            fileformat.read(xmlExample);
+
+            DigitalDocument digitalDocument = fileformat.getDigitalDocument();
+            DocStruct logical = digitalDocument.getLogicalDocStruct();
+            DocStruct physical = digitalDocument.getPhysicalDocStruct();
+
+            String strId = plug.getId(logical);
+
+            //add handles to each physical and logical element
+            plug.addHandle(logical, strId);
+            plug.addHandle(physical, strId);
+
+            fileformat.write(xmlOut);
+
+        } catch (Exception e) {
+
+            log.error(e.getMessage(), e);
+        }
+    }
+
     @Override
     public void initialize(Step step, String returnPath) {
+
         this.returnPath = returnPath;
         this.step = step;
         this.process = step.getProzess();
         prefs = process.getRegelsatz().getPreferences();
+        this.urn = prefs.getMetadataTypeByName("_urn");
     }
 
     @Override
     public boolean execute() {
 
-        XMLConfiguration config = ConfigPlugins.getPluginConfig(this);
-        String representativeElement = config.getString("RepresentativeStructureElement", TITLE_PAGE_NAME);
-        String errorMessage = config.getString("ErrorMessage", null);
-        String stepName = config.getString("StepName", null);
-
         try {
+            //read the metatdata
             Fileformat fileformat = process.readMetadataFile();
 
-            DigitalDocument dd = fileformat.getDigitalDocument();
-            DocStruct physical = dd.getPhysicalDocStruct();
-            DocStruct logical = dd.getLogicalDocStruct();
+            DigitalDocument digitalDocument = fileformat.getDigitalDocument();
+            DocStruct logical = digitalDocument.getLogicalDocStruct();
+            DocStruct physical = digitalDocument.getPhysicalDocStruct();
 
-            if (physical.getAllMetadata() != null && physical.getAllMetadata().size() > 0) {
-                for (Metadata md : physical.getAllMetadata()) {
-                    if (md.getType().getName().equals(REPRESENTATIVE_NAME)) {
-                        try {
-                            Integer value = new Integer(md.getValue());
-                            if (log.isTraceEnabled()) {
-                                log.trace("Found representative image with order " + value);
-                            }
-                            return true;
-                        } catch (Exception e) {
+            String strId = getId(logical);
 
-                        }
-                    }
-                }
-            }
+            //add handles to each physical and logical element
+            addHandle(logical, strId);
+            addHandle(physical, strId);
 
-            if (logical.getType().isAnchor()) {
-                logical = logical.getAllChildren().get(0);
-            }
-
-            DocStruct titlePage = null;
-            if (logical.getAllChildren() != null && !logical.getAllChildren().isEmpty()) {
-                for (DocStruct currentDocStruct : logical.getAllChildren()) {
-                    if (currentDocStruct.getType().getName().equals(representativeElement)) {
-                        titlePage = currentDocStruct;
-                        break;
-                    }
-                }
-            }
-
-            if (titlePage == null) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Found no title page in process " + process.getTitel());
-                }
-
-                if (errorMessage != null) {
-                    
-                    LogEntry logEntry = new LogEntry();
-                    logEntry.setContent(errorMessage);
-                    logEntry.setCreationDate(new Date());
-                    logEntry.setProcessId(process.getId());
-                    logEntry.setType(LogType.ERROR);
-                    
-                    ProcessManager.saveLogEntry(logEntry);
-                    
-//                    ProcessManager.addLogfile(WikiFieldHelper.getWikiMessage(process.getWikifield(), "error", errorMessage), process.getId());
-                }
-                if (stepName != null) {
-                    List<Step> previousSteps = StepManager.getSteps("Reihenfolge desc", " schritte.prozesseID = " + process.getId()
-                            + " AND Reihenfolge < " + step.getReihenfolge(), 0, Integer.MAX_VALUE);
-                    Step destination = null;
-                    for (Step currentStep : previousSteps) {
-                        if (currentStep.getTitel().equals(stepName)) {
-                            destination = currentStep;
-                            break;
-                        }
-                    }
-
-                    if (destination == null) {
-                        return false;
-                    }
-                    step.setEditTypeEnum(StepEditType.MANUAL_SINGLE);
-                    step.setBearbeitungsstatusEnum(StepStatus.LOCKED);
-                    step.setCorrectionStep();
-                    step.setBearbeitungsende(null);
-                    StepManager.saveStep(step);
-                    for (Step currentStep : previousSteps) {
-                        currentStep.setEditTypeEnum(StepEditType.MANUAL_SINGLE);
-                        currentStep.setBearbeitungsstatusEnum(StepStatus.LOCKED);
-                        currentStep.setCorrectionStep();
-                        currentStep.setBearbeitungsende(null);
-                        StepManager.saveStep(currentStep);
-                        if (currentStep.getTitel().equals(stepName)) {
-                            currentStep.setBearbeitungsstatusEnum(StepStatus.OPEN);
-                            StepManager.saveStep(currentStep);
-                            return false;
-                        }
-                    }
-
-                }
-
-                return true;
-            }
-
-            List<Reference> linkedImages = titlePage.getAllToReferences();
-
-            if (linkedImages == null || linkedImages.isEmpty()) {
-                return true;
-            }
-            DocStruct page = linkedImages.get(0).getTarget();
-            MetadataType no = prefs.getMetadataTypeByName(PAGE_NO_NAME);
-            List<? extends Metadata> pageNoList = page.getAllMetadataByType(no);
-            for (Metadata pageNo : pageNoList) {
-                if (log.isDebugEnabled()) {
-                    log.debug("First image of title page has order number " + pageNo.getValue());
-                }
-                MetadataType rep = prefs.getMetadataTypeByName(REPRESENTATIVE_NAME);
-
-                Metadata md = new Metadata(rep);
-                md.setValue(pageNo.getValue());
-                physical.addMetadata(md);
-                break;
-            }
-            if (log.isDebugEnabled()) {
-                log.debug("Save file with representative image.");
-            }
+            //and save the metadata again.
             process.writeMetadataFile(fileformat);
 
-        } catch (ReadException | PreferencesException | WriteException | IOException | InterruptedException | SwapException | DAOException
-                | MetadataTypeNotAllowedException e) {
-            log.error(e);
-            return false;
+        } catch (Exception e) {
+
+            log.error(e.getMessage(), e);
         }
 
-        return true;
+        return false;
+    }
+
+    private String getId(DocStruct logical) {
+
+        List<Metadata> lstMetadata = logical.getAllMetadata();
+
+        if (lstMetadata != null) {
+            for (Metadata metadata : lstMetadata) {
+                if (metadata.getType().getName().equals("CatalogIDDigital")) {
+                    return metadata.getValue();
+                }
+            }
+        }
+
+        //otherwise:
+        return null;
+    }
+
+    // check if Metadata handle exists
+    // if not, create handle and save it under "_urn" in the docstruct.
+    public void addHandle(DocStruct docstruct, String strId) throws HandleException, IOException, MetadataTypeNotAllowedException {
+
+        XMLConfiguration config = ConfigPlugins.getPluginConfig(title);
+        String strPEMFile = config.getString("PEMFile", PEM_FILE);
+        HandleClient handler = new HandleClient(strPEMFile);
+
+        if (docstruct.getAllChildren() != null) {
+            // run recursive through all children
+            for (DocStruct ds : docstruct.getAllChildren()) {
+                addHandle(ds, strId);
+            }
+        } else {
+
+            //already has a handle?
+            String strHandle = getHandle(docstruct);
+            if (strHandle == null) {
+                //if not, make one.
+                strHandle = handler.makeURLHandleForObject(strId);
+                setHandle(docstruct, strHandle);
+            }
+        }
+
+    }
+
+    //If the element already has a handle, return it, otherwise return null.
+    private String getHandle(DocStruct docstruct) {
+
+        List<? extends Metadata> lstURN = docstruct.getAllMetadataByType(urn);
+
+        if (lstURN.size() == 1) {
+            return lstURN.get(0).getValue();
+        }
+
+        //otherwise
+        return null;
+    }
+
+    //Add metadata to the element containing the handle.
+    private void setHandle(DocStruct docstruct, String strHandle) throws MetadataTypeNotAllowedException {
+
+        Metadata md = new Metadata(urn);
+        md.setValue(strHandle);
+        docstruct.addMetadata(md);
     }
 
     @Override
